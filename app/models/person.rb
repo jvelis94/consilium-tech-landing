c42a4c157f1c7a7b33d2a9c82e6197c8d12ed404
Joel Velis <joelvelis94@gmail.com>
2020-05-31 20:28:57 -0400

    made form dynamic based on user input

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,9 +1,12 @@
 class Person < ApplicationRecord
+    JOB_TITLE = ['Software Engineer', 'Data Science', 'DevOps', 'IT', 'Product', 'Management', 'Other'].freeze
+    
+
     validates :first_name, presence: true
     validates :last_name, presence: true
     validates :email, presence: true
     validates :city, presence: true
     validates :recent_company, presence: true
-    validates :expertise, presence: true
-    validates :job_title, presence: true, inclusion: { in: %w[Software Data DevOps IT Product Management Other] }
+    # validates :expertise, presence: true
+    validates :job_title, presence: true, inclusion: { in: JOB_TITLE }
 end
